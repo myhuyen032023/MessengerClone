629775343794c8944881e2948d62178a1bd5ba20
My Huyen <myhuyen032023@gmail.com>
2024-08-27 12:41:11 +0700

    Initial Commit

---
+++ MessengerClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessengerClone
+//
+//  Created by Hoang Thi My Huyen on 27/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Hoang Thi My Huyen on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
